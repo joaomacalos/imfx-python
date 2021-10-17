cac12c6d805213f35ea98b68b448c6b20964a20c
Joao Pedro <joaomacalos@gmail.com>
2021-10-17 16:02:35 +0200

    end session 2

--- utils_statsmodels.py
+++ utils_statsmodels.py
@@ -72,7 +72,7 @@ def dynamic_pred(model: RegressionResultsWrapper, exog: DataFrame, lag_endog: st
         ftbl['mean'].append(tbl['mean'].values[0])
         ftbl['mean_se'].append(tbl['mean_se'].values[0])
         ftbl['mean_ci_lower'].append(tbl['mean_ci_lower'].values[0])
-        ftbl['mean_ci_upper'].append(tbl['mean_ci_lower'].values[0])
+        ftbl['mean_ci_upper'].append(tbl['mean_ci_upper'].values[0])
         ftbl['obs_ci_lower'].append(tbl['obs_ci_lower'].values[0])
         ftbl['obs_ci_upper'].append(tbl['obs_ci_upper'].values[0])
 
@@ -84,3 +84,112 @@ def dynamic_pred(model: RegressionResultsWrapper, exog: DataFrame, lag_endog: st
     df_ftbl = DataFrame(ftbl).set_index(exog.index)
 
     return df_ftbl
+
+from numpy import zeros, dot, quantile, mean
+from numpy.random import choice
+from numpy.linalg import lstsq
+from statsmodels.api import OLS
+
+def stochastic_forecast(
+    model: RegressionResultsWrapper, 
+    X_train: DataFrame, 
+    X_test: DataFrame,
+    ci: float = .95, 
+    simulations: int = 1000) -> DataFrame:
+    """
+    Use bootstrapped residuals to perform stochastic forecasting.
+
+    params:
+    model: a fitted OLS object from `statsmodels`
+    X_train: the exogenous variables for the training set.
+    X_test: the exogenous variables for the forecasting period.
+    interval: the confidence interval.
+    simulations: the number of bootstrapped repetitions.
+    """
+    
+    X_train = X_train.copy()
+    X_test = X_test.copy()
+    
+    test_size = X_test.shape[0]
+
+    baseline = model.predict()
+    len_baseline = len(baseline)
+    residuals = model.resid
+    
+    X_train = X_train.rename(columns={"const": "Intercept"})
+    X_test = X_test.rename(columns={"const": "Intercept"})
+
+    # Boot
+    bootstraps = zeros((simulations, test_size))
+    for i in range(simulations):
+        # boot_params = [np.random.normal(mean, sd) for mean, sd in zip(params, params_se)]
+
+        boot_residuals = choice(residuals, len_baseline, replace=True)
+        boot_y = baseline + boot_residuals
+
+        params, *_ = lstsq(X_train, boot_y, rcond=None)
+
+        pred = dot(X_test, params)
+        
+        bootstraps[i, :] = pred
+
+    final_frame = zeros((test_size, 3))
+    
+    quantile_intervals = [.5 - (ci/2), .5, .5 + (ci/2)]
+
+    for i in range(test_size):
+        final_frame[i, :] = quantile(bootstraps[:, i], quantile_intervals)
+
+    final_frame = DataFrame(final_frame).set_index(X_test.index)
+    final_frame.columns = ['lower_q', 'median', 'upper_q']
+
+    return final_frame
+
+
+def stochastic_forecast2(
+    model, 
+    X_train, 
+    X_test,
+    ci=0.95,
+    simulations = 1000):
+        
+    X_train = X_train.copy()
+    X_test = X_test.copy()
+    
+    test_size = X_test.shape[0]
+
+    baseline = model.predict()
+    len_baseline = len(baseline)
+    residuals = model.resid
+    
+    X_train = X_train.rename(columns={"const": "Intercept"})
+    X_test = X_test.rename(columns={"const": "Intercept"})
+
+    X_train = X_train.assign(Intercept=1.)
+    X_test = X_test.assign(Intercept=1.)
+
+    # Boot
+    boot_mean = zeros((simulations, test_size))
+    boot_upper = zeros((simulations, test_size))
+    boot_lower = zeros((simulations, test_size))
+    for i in range(simulations):
+        # boot_params = [np.random.normal(mean, sd) for mean, sd in zip(params, params_se)]
+
+        boot_residuals = choice(residuals, len_baseline, replace=True)
+        boot_y = baseline + boot_residuals
+
+        pred = OLS(boot_y, X_train).fit().get_prediction(X_test).summary_frame(alpha=ci)
+
+        boot_mean[i, :] = pred['mean']
+        boot_upper[i, :] = pred['obs_ci_upper']
+        boot_lower[i, :] = pred['obs_ci_lower']
+
+    final_frame = zeros((test_size, 3))
+    
+    for i in range(test_size):
+        final_frame[i, :] = [mean(boot_mean[:, i]), mean(boot_lower[:, i]), mean(boot_upper[:, i])]
+
+    final_frame = DataFrame(final_frame).set_index(X_test.index)
+    final_frame.columns = ['mean', 'lower', 'upper']
+
+    return final_frame

422db895de10d8864e99998dafd1f3cc992ceab8
Joao Pedro <joaomacalos@gmail.com>
2021-10-20 09:17:42 +0200

    Add module 3B

--- utils_statsmodels.py
+++ utils_statsmodels.py
@@ -4,30 +4,31 @@ from statsmodels.regression.linear_model import RegressionResultsWrapper
 from pandas import Series, DataFrame
 
 
-def adf_test(series,title=''):
+def adf_test(series, terms='c', title=''):
     """
     Pass in a time series and an optional title, returns an ADF report
     """
     print(f'Augmented Dickey-Fuller Test: {title}')
-    result = adfuller(series.dropna(),autolag='AIC') # .dropna() handles differenced data
+    result = adfuller(series.dropna(),autolag='AIC', regression=terms) # .dropna() handles differenced data
     
     labels = ['ADF test statistic','p-value','# lags used','# observations']
     out = Series(result[0:4],index=labels)
 
     for key,val in result[4].items():
         out[f'critical value ({key})']=val
+
+    out['Terms'] = terms
         
     print(out.to_string())          # .to_string() removes the line "dtype: float64"
     
     if result[1] <= 0.05:
         print("Strong evidence against the null hypothesis")
-        print("Reject the null hypothesis")
         print("Data has no unit root and is stationary")
     else:
         print("Weak evidence against the null hypothesis")
-        print("Fail to reject the null hypothesis")
         print("Data has a unit root and is non-stationary")
 
+    
 def bglm_test(model, lags, title='') -> Series:
     """
     Pass in a fitted model and return the Breusch-Godfrey LM Test
